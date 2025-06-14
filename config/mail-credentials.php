7e98b3b276e82dae8d1c573f76899b1d3d875c07
paleanelo <kozhinaksenya2005@gmail.com>
2025-06-15 02:33:55 +0300

    Initial commit with vendor

---
+++ config/mail-credentials.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'smtpUsername' => 'dostavka.fruktov.spb@mail.ru',
+    'smtpPassword' => 'As0K1UhPqn5pggbAqC6M',
+];
---
+++ config/vk-token.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'vkUserToken' => 'vk1.a.TmZrAQJUtKoLQr2nKrcze_NhmJFJyOZOGMhpw2pme5nM2Kp1SGTHMYW7hJu_FMg_frUq_4TamH0TFNvcsTSs-htA6AWgS90U5POXsb3O4kHSTNs4px2BrqdNedDk7x-Vtc3hgcUcrX7Z0JP-OhdayAOkF9paiVEbBNuHwQ8s_TnBtKZBVQPKS0yYIFD4bG6-dYv2bi2kzQBb6PWBsfSXlg', // токен админа
+    'vkToken' => 'vk1.a.T8G7M-gnuQGQ_mKiKwpKQq07yEGGX7KFAUOhWNL_kr7anGyJK3pFBKmSFHKN_7zQ0_twPDrAdvZA-yY2E9VsUaXTaDm9nTQvYoiKJ2k1tjI6iHNMLs4bpsM8z85seBVZqV4pVqqYfQPauzbRTyKpOxJq_cC54SqUfisE70Q5LYdoN0N2b2G1ikiBgXYQtk5NEZhMfasSVhImVm-8_W6E4A', //токен группы
+    'vkAdminId' => 1052745120, // ID админа
+];
